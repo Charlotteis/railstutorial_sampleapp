ac761298fcfc73497fa88d96c887152d5b02df9d
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-09-29 15:47:10 +0100

    feat: finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    # Insecure to pass all the params to a User creation, can easily be
+    # manipulated by passing in own commands via curl, for instance.
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
